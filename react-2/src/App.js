d79217b475ca84a02133278aa8d5bc2414909c4b
Steven Gong <gonghaima@hotmail.com>
2023-04-19 15:50:58 +1000

    rename project 1

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+function App({ articles }) {
+  const [articlesOrdered, setArticlesOrdered] = useState(
+    articles.slice().sort((a, b) => b.upvotes - a.upvotes)
+  );
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
+  const mostVoted = () => {
+    setArticlesOrdered(
+      articlesOrdered.slice().sort((a, b) => b.upvotes - a.upvotes)
     );
+  };
 
+  const mostRecent = () => {
+    setArticlesOrdered(
+      articlesOrdered
+        .slice()
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+    );
+  };
+
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          onClick={mostVoted}
+          data-testid="most-upvoted-link"
+          className="small"
+        >
+          Most Upvoted
+        </button>
+        <button
+          onClick={mostRecent}
+          data-testid="most-recent-link"
+          className="small"
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={articlesOrdered} />
+    </div>
+  );
 }
 
 export default App;
